3da19401637832230abcc60759ae37914fa6ff73
Bennet Fauber <bennet@umich.edu>
2020-04-17 14:31:37 -0400

    Added some skeleton to redot
    - move the list of directories and files into version.py for importation
      (should rename that to redot-config.py or something)
    - checking to see if each directory exists
    - checking to see if each file exists
    - added importation of useful functions from path and datetime
    - constructing dated name of backup directory
    - adding the correct value for the user's home directory to the path names
    
    Output now looks like
    
    This is version 0.0 of the ARC-TS redot script
    
    Attempting to backup these directories to
      /home/bennet/redot/backups/2020-04-17_14:34:27
        /home/bennet/.ssh                   : exists
        /home/bennet/.mozilla               : exists
        /home/bennet/.mozilla/plugins       : exists
        /home/bennet/.mozilla/extensions    : exists
    
    Attempting to backup these files to
      /home/bennet/redot/backups/2020-04-17_14:34:27
        /home/bennet/.kshrc                 : exists
        /home/bennet/.bash_profile          : exists
        /home/bennet/.bash_logout           : exists
        /home/bennet/.emacs                 : exists
        /home/bennet/.bashrc                : exists
        /home/bennet/.zshrc                 : exists

--- version.py
+++ version.py
@@ -1,1 +1,17 @@
 version = '0.0'
+
+files = [
+    ".kshrc",
+    ".bash_profile",
+    ".bash_logout",
+    ".emacs",
+    ".bashrc",
+    ".zshrc",
+]
+
+directories = [
+    ".ssh",
+    ".mozilla",
+    ".mozilla/plugins",
+    ".mozilla/extensions",
+]
